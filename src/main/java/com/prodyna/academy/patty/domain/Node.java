60b337e288d019a711f67d158b380d7b26c1d53b
aheizenreder <aheizenreder@prodyna.com>
2013-12-14 08:26:14 +0100

    add hashCode, equals and toString to Node and its derevates.

--- src/main/java/com/prodyna/academy/patty/domain/Node.java
+++ src/main/java/com/prodyna/academy/patty/domain/Node.java
@@ -49,4 +49,53 @@ public abstract class Node {
 	public void accept(Visitor visitor) {
 
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		result = prime * result + (int) (size ^ (size >>> 32));
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Node other = (Node) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (parent == null) {
+			if (other.parent != null) {
+				return false;
+			}
+		} else if (!parent.equals(other.parent)) {
+			return false;
+		}
+		if (size != other.size) {
+			return false;
+		}
+		return true;
+	}
+	
 }
--- src/main/java/com/prodyna/academy/patty/domain/TextFile.java
+++ src/main/java/com/prodyna/academy/patty/domain/TextFile.java
@@ -38,4 +38,57 @@ public class TextFile extends File {
 	public void accept(final Visitor visitor) {
 		visitor.visit(this);
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "TextFile [textEncoding=" + textEncoding + ", pageCount="
+				+ pageCount + ", getName()=" + getName() + ", getSize()="
+				+ getSize() + ", getParent()=" + getParent() + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + (int) (pageCount ^ (pageCount >>> 32));
+		result = prime * result
+				+ ((textEncoding == null) ? 0 : textEncoding.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TextFile other = (TextFile) obj;
+		if (pageCount != other.pageCount) {
+			return false;
+		}
+		if (textEncoding == null) {
+			if (other.textEncoding != null) {
+				return false;
+			}
+		} else if (!textEncoding.equals(other.textEncoding)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
